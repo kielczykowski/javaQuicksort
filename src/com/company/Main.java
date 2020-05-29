b3877fa415b3b7e0307c79fb0eca366116ba4d43
mki <michal.k.kielczykowski@gmail.com>
2020-05-29 13:55:25 +0200

    ForkJoin done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,31 @@
 package com.company;
 
 import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
 
     public static void main(String[] args) {
         //losowa tablica liczb całkowitych
-        int size = 100;
+        int size = 10000000;
         int[] intArray = new int[size];
 
         Random rand = new Random();
         for (int i=0; i<size; i++){
-            intArray[i] = rand.nextInt((100 - 0) + 1) + 0;
+            intArray[i] = rand.nextInt();
             //System.out.println(intArray[i]);
         }
+        Quicksort qs = new Quicksort(intArray);
+        // System.out.println("Before Sort: ");
+        // qs.print();
 
-        int[] copiedArray1 = intArray.clone();
-        int[] copiedArray2 = intArray.clone();
+        ForkJoinPool fjp = new ForkJoinPool();
+        fjp.invoke(qs);
 
-        //Wywołanie QUICKSORT jednowątkowo
-        Quicksort.quickSort(intArray);
+        fjp.shutdown();
 
+        // System.out.println("After Sort: ");
+        // qs.print();
 
 
     }
--- src/com/company/Quicksort.java
+++ src/com/company/Quicksort.java
@@ -1,7 +1,27 @@
 package com.company;
+import java.util.concurrent.RecursiveAction;
 
-public class Quicksort {
-    static int run(int array[],int low,int high){
+public class Quicksort extends RecursiveAction  {
+    private int[] array;
+    private int low;
+    private int high;
+
+    public Quicksort(int[] array)
+    {
+        this.array = array;
+        this.low = 0;
+        this.high = array.length - 1;
+    }
+
+    public Quicksort(int[] array, int low, int high)
+    {
+        this.array = array;
+        this.low = low;
+        this.high = high;
+    }
+
+
+    private int run(int array[],int low,int high){
 
         int pivot = array[high];
         int i = low-1;
@@ -23,29 +43,33 @@ public class Quicksort {
         return i+1;
     }
 
-
-    static void sort(int array[], int low, int high){
+    @Override
+    protected void compute(){
         if(low < high){
             int pi = run(array, low, high);
-            sort(array, low, pi-1);
-            sort(array, pi+1, high);
+            invokeAll(new Quicksort(array, low, pi-1), new Quicksort(array, pi+1, high));
         }
 
     }
 
     //wypisanie tablicy
-    static void print(int array[]){
-        System.out.println("SORTED: ");
+    public void print(int array[]){
         for(int i=0; i < array.length; i++){
             System.out.println(array[i]);
         }
     }
+
+    public void print(){
+        for(int i=0; i < this.array.length; i++){
+            System.out.println(this.array[i]);
+        }
+    }
     //klasa main obiektu QUICKSORT
-    static void quickSort(int array[]){
+    private void quickSort(int array[]){
         int low = 0;
         int high = array.length-1;
 
-        sort(array,low,high);
+        run(array,low,high);
         print(array);
 
     }
